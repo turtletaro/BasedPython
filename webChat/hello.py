3ce855afc71d6172236b9d3d82d1a5026fc4c108
turtletaro <turtletaro@126.com>
2018-11-18 21:19:41 +0800

    fixed

--- webChat/hello.py
+++ webChat/hello.py
@@ -1,6 +1,7 @@
 import itchat
 import sys
 import codecs
+import re
 
 #print(sys.stdout.encoding)
 #sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
@@ -12,3 +13,16 @@ user = itchat.get_friends()
 #get the first user for yourself
 i = str(user[:1])
 print(i)
+
+info = i[8:-2]
+print(info)
+
+name = str(re.findall(r"'NickName': '(.+?)'", info))[2:-2]
+sign = str(re.findall(r"'Signature': '(.+?)'", info))[2:-2]
+sex = str(re.findall(r"'Sex': (.+?),", info))[2:-2]
+uin = str(re.findall(r"'Uin': (.+?),", info))[2:-2]
+print(name)
+print(sign)
+print(sex)
+print(uin)
+

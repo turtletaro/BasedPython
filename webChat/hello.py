3474e793ef603e7a08952b17f0787caf8dfd68e4
turtletaro <turtletaro@126.com>
2018-11-18 22:11:31 +0800

    fixed

--- webChat/hello.py
+++ webChat/hello.py
@@ -10,7 +10,7 @@ from pandas import DataFrame
 #print(sys.stdout.encoding)
 #sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 ###print(sys.stdout.encoding)
-#itchat.auto_login() 
+itchat.auto_login() 
 itchat.send('hello webChat',toUserName='filehelper')
 user = itchat.get_friends()
 
@@ -32,7 +32,7 @@ print(uin)
 
 friends = itchat.get_friends(update=True)[0:]
 male = female = other = 0 
-#friends[0]是自己的信息，所以要从friends[1]开始 
+#friends from 1 
 for i in friends[1:]: 
     sex = i["Sex"] 
     if sex == 1: 
@@ -41,9 +41,9 @@ for i in friends[1:]:
         female += 1 
     else: 
         other +=1 
-#计算朋友总数 
+#count friends
 total = len(friends[1:]) 
-#打印出自己的好友性别比例 
+
 print("男性好友： %.2f%%" % (float(male)/total*100) + "\n" + 
 "女性好友： %.2f%%" % (float(female) / total * 100) + "\n" + 
 "不明性别好友： %.2f%%" % (float(other) / total * 100))

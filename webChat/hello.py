f1aad6d2c06516a04ee9a6433e264f473dfcb13d
turtletaro <turtletaro@126.com>
2018-11-18 20:46:59 +0800

    fixed

--- webChat/hello.py
+++ webChat/hello.py
@@ -1,3 +1,14 @@
 import itchat
-itchat.auto_login()
-itchat.send('Hello, filehelper', toUserName='filehelper')
+import sys
+import codecs
+
+#print(sys.stdout.encoding)
+#sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
+###print(sys.stdout.encoding)
+#itchat.auto_login() 
+itchat.send('hello webChat',toUserName='filehelper')
+user = itchat.get_friends()
+
+#get the first user for yourself
+i = str(user[:1])
+print(i)
